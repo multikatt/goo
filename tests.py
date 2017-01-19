fa964e0c7608381d0d7249f9f93f036eea4bf298
multikatt <github@kattfest.se>
2017-01-18 21:24:07 -0500

    tests init

---
+++ tests.py
@@ -0,0 +1,14 @@
+import app
+import os
+import unittest
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
