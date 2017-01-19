04e130e2610d35f393e2d8d3b1141ef9f82903be
multikatt <github@kattfest.se>
2017-01-19 00:20:35 -0500

    updated

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+import os
+
 app = Flask(__name__)
+
+app.config.update(
+    DB_PATH=os.getcwd()+"/db.sql"
+)
+
+
+def create_db():
+    db_path = app.config["DB_PATH"]
+    g.db = sqlite3.connect(db_path)
+    g.db.commit()
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db_path = app.config["DB_PATH"]
+    if hasattr(g, "db"):
+        g.db.close()
+        os.unlink(db_path)
+
 
 @app.route("/")
 def hello():
     return "hello"
+
 
 if __name__ == "__main__":
     app.run()
--- tests.py
+++ tests.py
@@ -1,14 +1,22 @@
 import app
-import os
 import unittest
+import tempfile
+
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.app = app.app.test_client()
+        with app.app.app_context():
+            app.app.config["DB_PATH"] = tempfile.mktemp()
+            app.create_db()
 
     def tearDown(self):
-        pass
+        with app.app.app_context():
+            app.close_db(0)
+
+    def test_passthis(self):
+        self.assertEqual(1, 1)
 
 if __name__ == "__main__":
     unittest.main()
